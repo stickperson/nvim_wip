19888d28b94e809e297743ce77a72fa6f110885c
Joe Meissler <jmeissler@cedar.com>
2023-01-23 20:18:19 -0800

    mini.indentscope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -293,6 +293,24 @@ local plugins = {
 
   -- Improved UI hooks. E.g. lsp rename
   ["stevearc/dressing.nvim"] = {},
+
+  ["echasnovski/mini.indentscope"] = {
+    event = "BufReadPre",
+    config = function()
+      local opts = {
+        -- symbol = "▏",
+        symbol = "│",
+        options = { try_as_border = true },
+      }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+      require("mini.indentscope").setup(opts)
+    end,
+  },
 }
 
 local final_table = {}
